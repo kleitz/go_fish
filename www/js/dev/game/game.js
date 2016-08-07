99735bd4ca1a0dcd2f05690de99b70ddd6a0b7c0
Matias Salas <mat.salas@comcast.net>
2016-08-06 23:26:43 -0700

    set game view ratio to mobile device

--- www/js/dev/game/game.js
+++ www/js/dev/game/game.js
@@ -1,3 +1,3 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameView')
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, (window.innerHeight * window.devicePixelRatio) - 45 * window.devicePixelRatio, Phaser.AUTO, 'gameView')
 
 game.state.add('Game', GameState, true)
