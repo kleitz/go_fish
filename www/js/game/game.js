152c669d83bf0ce5020a6b5ec8dc5fdc64f65510
Matias Salas <mat.salas@comcast.net>
2016-09-18 21:52:09 -0700

    remove test game assets

--- www/js/game/game.js
+++ www/js/game/game.js
@@ -4,30 +4,16 @@ function GameState (game) {
 
 //  Phaser preload function to preload all game assets.
 GameState.prototype.preload = function () {
-  this.game.load.image('sky', '../../img/assets/sky.png')
-  this.game.load.image('ground', '../../img/assets/platform.png')
-  this.game.load.image('star', '../../img/assets/star.png')
+  // load backgound image
+  this.game.load.image('background', '../../assets/images/background.png')
 }
 
 //  Phaser Create function to instantiate all game assets.
 GameState.prototype.create = function () {
   game.physics.startSystem(Phaser.Physics.ARCADE)
 
-  // add a simple sky
-  game.add.sprite(0, 0, 'sky')
-  level_1.platforms = game.add.group()
-  level_1.platforms.enableBody = true
-
-  level_1.ground = level_1.platforms.create(0, game.world.height - 128, 'ground')
-  //scale the ground to fit the screen
-  level_1.ground.scale.setTo(2,2)
-  level_1.ground.body.immovable = true
-
-  level_1.ledge_1 = level_1.platforms.create(200, 400, 'ground')
-  level_1.ledge_1.body.immovable = true
-  level_1.ledge_2 = level_1.platforms.create(-150, 250, 'ground')
-  level_1.ledge_2.body.immovable = true
-  this.game.add.sprite(0, 0, 'star')
+  // add a background
+  game.add.sprite(0,0, 'background')
 
 }
 
@@ -37,7 +23,7 @@ GameState.prototype.update = function () {
 }
 
 //  instantiate a new Phaser Game object.
-var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, (window.innerHeight * window.devicePixelRatio) - 45 * window.devicePixelRatio, Phaser.AUTO, 'gameView')
+var game = new Phaser.Game(640, 360, Phaser.AUTO, 'gameView')
 
 function GameLevel () {
 }
